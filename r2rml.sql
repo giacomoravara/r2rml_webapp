a9b98d6a792124f079d351a9f6df55b5db56ddfd
giacomoravara <60889173+giacomoravara@users.noreply.github.com>
2021-02-01 18:45:21 +0100

    Update r2rml.sql

--- r2rml.sql
+++ r2rml.sql
@@ -1,141 +1,73 @@
--- phpMyAdmin SQL Dump
--- version 4.2.10
--- http://www.phpmyadmin.net
---
--- Host: localhost:3306
--- Generation Time: May 10, 2016 at 08:47 PM
--- Server version: 5.5.38
--- PHP Version: 5.6.2
+/*
+Navicat MySQL Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+Source Server         : local
+Source Server Version : 50525
+Source Host           : localhost:3306
+Source Database       : test
 
---
--- Database: `r2rml`
---
+Target Server Type    : MYSQL
+Target Server Version : 50525
+File Encoding         : 65001
 
--- --------------------------------------------------------
+Date: 2015-05-24 20:19:59
+*/
 
---
--- Table structure for table `DEPT`
---
+SET FOREIGN_KEY_CHECKS=0;
 
-CREATE TABLE `DEPT` (
-  `DEPTNO` int(11) NOT NULL,
-  `DNAME` varchar(30) DEFAULT NULL,
-  `LOC` varchar(30) DEFAULT NULL
+-- ----------------------------
+-- Table structure for `person`
+-- ----------------------------
+DROP TABLE IF EXISTS `person`;
+CREATE TABLE `person` (
+  `ID` int(11) NOT NULL,
+  `NAME` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+-- ----------------------------
+-- Records of person
+-- ----------------------------
+INSERT INTO `person` VALUES ('1', 'Charles');
+INSERT INTO `person` VALUES ('2', 'Louis');
+INSERT INTO `person` VALUES ('3', 'Mark');
+INSERT INTO `person` VALUES ('4', 'Patrick');
+
+-- ----------------------------
+-- Table structure for `professor`
+-- ----------------------------
+DROP TABLE IF EXISTS `professor`;
+CREATE TABLE `professor` (
+  `ID` int(11) NOT NULL,
+  `TITLE` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `person_fk` FOREIGN KEY (`ID`) REFERENCES `person` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `DEPT`
---
+-- ----------------------------
+-- Records of professor
+-- ----------------------------
+INSERT INTO `professor` VALUES ('4', 'PhD');
 
-INSERT INTO `DEPT` (`DEPTNO`, `DNAME`, `LOC`) VALUES
-(10, 'APPSERVER', 'NEW YORK');
+-- ----------------------------
+-- Table structure for `student`
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student` (
+  `ROLLNO` int(11) NOT NULL,
+  `DEGREE` varchar(50) DEFAULT NULL,
+  `ID` int(11) NOT NULL,
+  PRIMARY KEY (`ROLLNO`),
+  KEY `fk_student` (`ID`),
+  CONSTRAINT `fk_student` FOREIGN KEY (`ID`) REFERENCES `person` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+INSERT INTO `student` VALUES ('100', 'ungraduated', '1');
+INSERT INTO `student` VALUES ('200', 'graduated', '2');
+INSERT INTO `student` VALUES ('300', 'master', '3');
 
---
--- Table structure for table `DEPT2`
---
+-- ----------------------------
 
-CREATE TABLE `DEPT2` (
-  `DEPTNO` int(11) DEFAULT NULL,
-  `DNAME` varchar(30) DEFAULT NULL,
-  `LOC` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `DEPT2`
---
-
-INSERT INTO `DEPT2` (`DEPTNO`, `DNAME`, `LOC`) VALUES
-(10, 'APPSERVER', 'NEW YORK'),
-(20, 'RESEARCH', 'BOSTON');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `EMP`
---
-
-CREATE TABLE `EMP` (
-  `EMPNO` int(11) NOT NULL,
-  `ENAME` varchar(100) DEFAULT NULL,
-  `JOB` varchar(20) DEFAULT NULL,
-  `DEPTNO` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `EMP`
---
-
-INSERT INTO `EMP` (`EMPNO`, `ENAME`, `JOB`, `DEPTNO`) VALUES
-(7369, 'SMITH', 'CLERK', 10);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `EMP2`
---
-
-CREATE TABLE `EMP2` (
-  `EMPNO` int(11) DEFAULT NULL,
-  `ENAME` varchar(100) DEFAULT NULL,
-  `JOB` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `EMP2`
---
-
-INSERT INTO `EMP2` (`EMPNO`, `ENAME`, `JOB`) VALUES
-(7369, 'SMITH', 'CLERK'),
-(7369, 'SMITH', 'NIGHTGUARD'),
-(7400, 'JONES', 'ENGINEER');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `EMP2DEPT`
---
-
-CREATE TABLE `EMP2DEPT` (
-  `EMPNO` int(11) DEFAULT NULL,
-  `DEPTNO` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `EMP2DEPT`
---
-
-INSERT INTO `EMP2DEPT` (`EMPNO`, `DEPTNO`) VALUES
-(7369, 10),
-(7369, 20),
-(7400, 10);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `DEPT`
---
-ALTER TABLE `DEPT`
- ADD PRIMARY KEY (`DEPTNO`);
-
---
--- Indexes for table `EMP`
---
-ALTER TABLE `EMP`
- ADD PRIMARY KEY (`EMPNO`), ADD KEY `DEPTNO` (`DEPTNO`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `EMP`
---
-ALTER TABLE `EMP`
-ADD CONSTRAINT `emp_ibfk_1` FOREIGN KEY (`DEPTNO`) REFERENCES `DEPT` (`DEPTNO`);
